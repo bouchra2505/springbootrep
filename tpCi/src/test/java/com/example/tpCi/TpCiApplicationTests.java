3aac138bc0773d0c7288f0a874a05b630ff81edf
Bouchra <bouchra.arif@univ-constantine2.dz>
2025-11-19 08:08:02 +0100

    initial commit

---
+++ tpCi/src/test/java/com/example/tpCi/TpCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpCi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
